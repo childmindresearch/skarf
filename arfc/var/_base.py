Fix sklearn estimator checks for covariance var

arfc/var/_base.py
@@ -1,13 +1,20 @@
 from abc import ABCMeta, abstractmethod
+from numbers import Integral
 from typing import NamedTuple, Self
 
 import numpy as np
 from numpy.random import RandomState
 from sklearn.base import BaseEstimator
 from sklearn.metrics import r2_score
-from sklearn.utils.validation import check_is_fitted, check_random_state
+from sklearn.utils.validation import (
+    check_is_fitted,
+    check_random_state,
+    validate_data,
+    _check_sample_weight,
+)
+from sklearn.utils._param_validation import Interval
 
-from ._utils import _tstride, _segments_to_windows
+from ._utils import _tstride, _segments_to_windows, _check_segments
 
 
 class BaseVAR(BaseEstimator, metaclass=ABCMeta):
@@ -34,12 +41,32 @@ class BaseVAR(BaseEstimator, metaclass=ABCMeta):
         Estimated coefficients for the VAR model. The terms are ordered by increasing
         lag.  The `i`th row of each term contains the prediction coefficients for the
         `i`th feature.
+
+    n_features_in_ : int
+        Number of features seen during :term:`fit`.
+
+    feature_names_in_ : array of shape (n_features_in_,)
+        Names of features seen during `fit`. Defined only when `X` has feature names
+        that are all strings.
     """
+
+    _parameter_constraints = {
+        "order": [Interval(Integral, 1, None, closed="left")],
+        "lag": [Interval(Integral, 0, None, closed="left")],
+        "random_state": ["random_state"],
+    }
 
     coef_: np.ndarray
     """Array of VAR coefficients of shape (order, n_targets, n_features)."""
 
+    n_features_in_: int
+    """Number of features seen during `fit`."""
+
+    feature_names_in_: np.ndarray
+    """Names of features seen during `fit`. Defined only when `X` has feature names."""
+
     scoring_function = staticmethod(r2_score)
+    """Static scoring function (default `r2_score`)"""
 
     def __init__(
         self,
@@ -98,6 +125,7 @@ class BaseVAR(BaseEstimator, metaclass=ABCMeta):
         X_pred : array-like of shape (n_samples, n_features)
         """
         check_is_fitted(self)
+        X = validate_data(self, X, reset=False)
         X_stride = _tstride(X, order=self.order, mode="same")
         return self._predict_strided(X_stride)
 
@@ -134,6 +162,12 @@ class BaseVAR(BaseEstimator, metaclass=ABCMeta):
         score: float
             Mean VAR prediction score (by default R2, see `scoring_function`).
         """
+        check_is_fitted(self)
+        if y is not None:
+            X, y = validate_data(self, X, y, reset=False)
+        else:
+            X = validate_data(self, X, reset=False)
+
         X_stride, y_shift, _, sample_weight_shift, _ = _preprocess_data(
             X,
             y=y,
@@ -165,6 +199,9 @@ class BaseVAR(BaseEstimator, metaclass=ABCMeta):
         X_samples : array-like of shape (n_samples, n_features)
         """
         check_is_fitted(self)
+        if X_init is not None:
+            X_init = validate_data(self, X_init, reset=False)
+
         if self.coef_.shape[1] != self.coef_.shape[2]:
             raise RuntimeError("Sampling requires n_targets == n_features.")
         if self.lag == 0:
@@ -219,6 +256,10 @@ def _preprocess_data(
     Sample weights are also temporally expanded so that all samples whose sliding
     prediction window overlaps with an excluded sample are also excluded.
     """
+    if segments is not None:
+        segments = _check_segments(segments, X=X)
+    if sample_weight is not None:
+        sample_weight = _check_sample_weight(sample_weight, X=X)
     if sample_weight is not None:
         if not np.allclose(sample_weight, sample_weight > 0):
             raise ValueError("Only binary sample_weight supported.")

arfc/var/covariance.py
@@ -1,21 +1,34 @@
+from copy import deepcopy
+from numbers import Integral, Real
 from typing import Literal, Self
 
 import numpy as np
 from numpy.random import RandomState
 from scipy.linalg import block_diag
-from sklearn.base import MetaEstimatorMixin
+from sklearn.base import MetaEstimatorMixin, clone, _fit_context
+from sklearn.utils.validation import validate_data
+from sklearn.utils._param_validation import HasMethods, Interval, StrOptions
 from sklearn.covariance import EmpiricalCovariance
 from sklearn.utils.validation import check_is_fitted
 
 from ._base import BaseVAR, _preprocess_data
 
+# TODO:
+# - [x] fix score reject y
+# - [] use sklearn.utils.validation.validate_data
+# - [] add docs for generated attributes like n_features_in_
+# - [] don't mutate estimator
+# - [] fix inheritance order (done)
+# - [] accept series inputs
+# - .... rest of the checks
 
-class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
+
+class CovarianceVAR(MetaEstimatorMixin, BaseVAR):
     """Covariance based VAR model.
 
     This model fits a linear VAR model parameterized by an underlying covariance matrix.
     The coefficients of the VAR model are represented as a learned polynomial of the
-    covariance coefficients::
+    covariance coefficjients::
 
         A[l] = sum(b[l, i] * C ** (i + 1) for i in range(degree))
 
@@ -25,7 +38,7 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
     Parameters
     ----------
     estimator : estimator object
-        Covariance estimator object implementing `fit()` and having a `covariance_`
+        `Covariance` estimator object implementing `fit()` and having a `covariance_`
         attribute.
 
     order : int, default=1
@@ -38,11 +51,19 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
     degree : int, default=3
         Degree of the polynomial re-parameterization.
 
+    use_precision : bool, default=False
+        Use the covariance estimator's precision (inverse covariance) matrix.
+
     alpha : float, default=None
         Ridge regression penalty parameter on the polynomial coefficients.
 
-    use_precision : bool, default=False
-        Use the covariance estimator's precision (inverse covariance) matrix.
+    mode : {'full', 'leave_one_out'}, default='full'
+        Model fit mode:
+
+        * 'full' : Use the full covariance.
+
+        * 'leave_one_out' : Zero the diagonal of the covariance before model fitting. This
+            prevents autocorrelation from improving model fit.
 
     random_state : int, RandomState instance, default=None
         The seed of the pseudo random number generator used when sampling.
@@ -57,6 +78,10 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         lag.  The `i`th row of each term contains the prediction coefficients for the
         `i`th feature.
 
+    estimator_ : Estimator object
+        Fit covariance estimator. If `frozen = True`, then the `estimator` parameter
+        must already be fit, and a deep copy is made.
+
     beta_ : array of shape (order, degree)
         Array of polynomial coefficients.
 
@@ -64,9 +89,20 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         Rank of the polynomial regression design matrix.
 
     singular_ : array of shape (order * degree,)
-        Singular values of the d
+        Singular values of the design matrix.
     """
 
+    _parameter_constraints = {
+        **BaseVAR._parameter_constraints,
+        "estimator": [HasMethods(["fit"])],
+        "degree": [Interval(Integral, 1, None, closed="left")],
+        "use_precision": ["boolean"],
+        "alpha": [Interval(Real, 0, None, closed="neither"), None],
+        "mode": [StrOptions({"full", "leave_one_out"})],
+    }
+
+    estimator_: EmpiricalCovariance
+    """Fit covariance estimator."""
     beta_: np.ndarray
     """Array of polynomial coefficients, shape (order, degree)."""
     rank_: int
@@ -81,7 +117,7 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         lag: int = 1,
         degree: int = 3,
         alpha: float | None = None,
-        mode: Literal["joint", "leave_one_out"] = "leave_one_out",
+        mode: Literal["full", "leave_one_out"] = "leave_one_out",
         use_precision: bool = False,
         frozen: bool = False,
         random_state: int | RandomState | None = None,
@@ -94,6 +130,7 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         self.use_precision = use_precision
         self.frozen = frozen
 
+    @_fit_context(prefer_skip_nested_validation=True)
     def fit(
         self,
         X: np.ndarray,
@@ -123,9 +160,7 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         self : object
             Returns the instance itself.
         """
-        if self.mode not in {"joint", "leave_one_out"}:
-            raise ValueError(f"Invalid mode '{self.mode}'.")
-
+        X = validate_data(self, X)
         X_stride, y_shift, _, sample_weight_shift, _ = _preprocess_data(
             X,
             y=None,
@@ -143,18 +178,20 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
 
         if self.frozen:
             check_is_fitted(self.estimator)
-            if self.estimator.covariance_.shape[1] != X.shape[1]:
+            estimator = deepcopy(self.estimator)
+            if estimator.covariance_.shape[1] != X.shape[1]:
                 raise ValueError(
                     "Shape of frozen covariance estimator doesn't match input data X"
                 )
         else:
-            self.estimator.fit(X)
+            estimator = clone(self.estimator)
+            estimator.fit(X)
 
         if self.use_precision:
-            mat = self.estimator.get_precision()
+            mat = estimator.get_precision()
         else:
-            mat = self.estimator.covariance_
-        mat = _preprocess_covariance(mat, with_diagonal=self.mode == "joint")
+            mat = estimator.covariance_
+        mat = _preprocess_covariance(mat, with_diagonal=self.mode == "full")
 
         # pre-compute polynomial ar terms
         pow_mats = np.stack([mat**deg for deg in range(1, self.degree + 1)])
@@ -181,11 +218,43 @@ class CovarianceVAR(BaseVAR, MetaEstimatorMixin):
         beta = beta.reshape((self.order, self.degree))
         coef = np.einsum("pq,qcd->pcd", beta, pow_mats)
 
+        self.estimator_ = estimator
         self.beta_ = beta
         self.rank_ = rank
         self.singular_ = singular_values
         self.coef_ = coef
         return self
+
+    def score(
+        self,
+        X: np.ndarray,
+        y: None = None,
+        segments: np.ndarray | None = None,
+        sample_weight: np.ndarray | None = None,
+    ) -> np.ndarray:
+        """Return the prediction score for the model (by default R2).
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training multivariate time series.
+
+        y : Ignored
+            Ignored.
+
+        segments : array-like of shape (n_samples,)
+            Indicator array of contiguous temporal segments in `X`.
+
+        sample_weight : float or array-like of shape (n_samples,), default=None
+            Sample weights. Only binary sample weights indicating time points to
+            include/exclude are currently supported.
+
+        Returns
+        -------
+        score: float
+            Mean VAR prediction score (by default R2, see `scoring_function`).
+        """
+        return super().score(X, y=None, segments=segments, sample_weight=sample_weight)
 
 
 def _preprocess_covariance(
@@ -200,7 +269,7 @@ def _preprocess_covariance(
 
     mat = np.where(np.isnan(covariance), 0.0, covariance)
     if not with_diagonal:
-        np.fill_diagonal(mat, 0.0)  # ignore diagonal
-    mat = mat / np.max(np.abs(mat))  # scale values to [-1, 1]
+        np.fill_diagonal(mat, 0.0)
+    mat = mat / (np.max(np.abs(mat)) + np.finfo(mat.dtype).eps)
     mat = np.ascontiguousarray(mat)
     return mat